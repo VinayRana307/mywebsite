2e0422ac401bb47735ef3641b43ce062e49d286f
VinayRana307 <vinay6365355@gmail.com>
2020-09-03 09:06:25 +0530

    Updates

---
+++ precache-manifest.4ec6c66434b74aaac05138c928d4a0b1.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e838c6645085a83364f25156775b1c5",
+    "url": "/mywebsite/index.html"
+  },
+  {
+    "revision": "0ff3223a2f1119671a3d",
+    "url": "/mywebsite/static/css/2.54b19870.chunk.css"
+  },
+  {
+    "revision": "85617144de6e9d4290e4",
+    "url": "/mywebsite/static/css/main.56975ef5.chunk.css"
+  },
+  {
+    "revision": "0ff3223a2f1119671a3d",
+    "url": "/mywebsite/static/js/2.3b846161.chunk.js"
+  },
+  {
+    "revision": "d7b7835b607d11dfb31dd586a58a01e3",
+    "url": "/mywebsite/static/js/2.3b846161.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85617144de6e9d4290e4",
+    "url": "/mywebsite/static/js/main.756d2797.chunk.js"
+  },
+  {
+    "revision": "5bd25a13385a463a54aa",
+    "url": "/mywebsite/static/js/runtime-main.abaf77ec.js"
+  },
+  {
+    "revision": "5c066d3f4060ea9fb7aa8fbce184c62d",
+    "url": "/mywebsite/static/media/banner.5c066d3f.jpg"
+  },
+  {
+    "revision": "ddfc57237da567f14343538ce9403186",
+    "url": "/mywebsite/static/media/image1.ddfc5723.jpg"
+  },
+  {
+    "revision": "1311e5d6f2d5539c1b7fe68036ce4029",
+    "url": "/mywebsite/static/media/image2.1311e5d6.jpg"
+  },
+  {
+    "revision": "ed1024fbfe2b6676eb74ec8bfe3cae17",
+    "url": "/mywebsite/static/media/image3.ed1024fb.jpg"
+  },
+  {
+    "revision": "a136f653ae4a8face77c0151708303dc",
+    "url": "/mywebsite/static/media/image4.a136f653.jpg"
+  },
+  {
+    "revision": "ddfc57237da567f14343538ce9403186",
+    "url": "/mywebsite/static/media/image5.ddfc5723.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mywebsite/static/media/logo.5d5d9eef.svg"
+  }
+]);
